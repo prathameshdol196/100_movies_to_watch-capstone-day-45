876ba47c848a207fa8a5a2aceffc2ca3027fa74f
prathamesh <prathameshdol196@gmail.com>
2022-07-19 20:11:13 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+webpage = response.text
+
+soup = BeautifulSoup(webpage, "html.parser")  # created a soup
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+all_movies_names = [movie.getText() for movie in all_movies]  # putting all movies names into all_movies_list list
+
+# for movie in movies:
+#     movie_name = movie.getText()
+#     movies_names.append(movie_name)
+
+all_movies_names.reverse()  # reversed a list
+
+with open("movies.txt", "w") as file:  # created file and writing movies names in it
+    for movie in all_movies_names:
+        file.write(f"{movie} \n")
+
